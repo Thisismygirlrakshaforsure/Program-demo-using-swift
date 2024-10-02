6020c7cdc595617d93c966099da790c9dadf3544
Raksha Adhikari <156924801+Thisismygirlrakshaforsure@users.noreply.github.com>
2024-10-02 23:10:14 +0545

    Create_This_program_demonstrates_arithmetic_operations.swift

---
+++ This_program_demonstrates_arithmetic_operations.swift
@@ -0,0 +1,13 @@
+var a: Int
+var b: Int
+
+a = 3
+b = 2
+
+print("a =", a)
+print("b =", b)
+print("a + b =", (a + b))
+print("a - b =", (a - b))
+print("a * b =", a * b)
+print("a / b =", a / b)
+print("a % b =", (a % b))
